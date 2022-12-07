44c725ecec8fd1cd10c0b46c8140ee32a923d396
Andreen-Muchiri <andreenshii@gmail.com>
2022-12-07 12:57:09 +0300

    Add show controller action

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,10 +1,19 @@
 class RoomsController < ApplicationController
 
-    def index
-        def index
+    # GET/rooms
+     def index
             rooms = Room.all
             render json: rooms, status: :ok
-        end
-    
     end
+
+    # GET/room/:id
+    def show
+   room = Room.find(params[:id])
+   if room
+    render json: room, status: :ok
+   else
+    render json: {error: "Room Not Found"}, status: :404
+   end
+end
+    
 end
