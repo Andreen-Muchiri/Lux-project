efad724e0c1c8ef6f85743b8d995a79cacba2d3a
Andreen-Muchiri <andreenshii@gmail.com>
2022-12-08 02:46:33 +0300

    Add routing

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,2 +1,23 @@
 class HotelsController < ApplicationController
+     # POST/hotel/:id
+     def create
+     hotel = Hotel.create(hotel_params)
+      render json: room, status: :created
+    end
+
+    # DESTROY/hotel/:id
+    def destroy
+    hotel = Hotel.find_by(id: params[:id])
+    if hotel
+    hotel.destroy
+    head :no_content
+    else
+        render json: {error: "Room not found"}, status: :not_found
+    end
+
+end
+    private
+ def hotel_params
+    params.permit(:city, :name, :room_type, :price)
+end
 end
--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,2 +1,4 @@
 class Hotel < ApplicationRecord
+
+   
 end
