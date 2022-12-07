c80bc7335dc61121a31ed539a15c73e64451f891
Andreen-Muchiri <andreenshii@gmail.com>
2022-12-08 02:01:02 +0300

    Add changes to home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,28 @@
-import React from "react"
+import React, {useState} from "react"
 import Heading from "./Heading"
 import "./home.css"
 
 
 
 function Home(){
+  const [selectedCity, setSelectedCity] = useState('');
+  const [selectedRoomType, setSelectedRoomType] = useState('');
+  const [selectedPrice, setSelectedPrice] = useState(''); 
+
+  const handleCityChange = (event) => {
+    // Update the state variable when the user changes the selection 
+    setSelectedCity(event.target.value);
+  };
+
+  const handleRoomTypeChange = (event) => {
+    // Update the state variable when the user changes the selection 
+    setSelectedRoomType(event.target.value);
+  };
+  const handlePriceChange = (event) => {
+    // Update the state variable when the user changes the selection 
+    setSelectedPrice(event.target.value);
+  };
+
     return (
         <>
 <section className='home'>
@@ -14,16 +32,18 @@ function Home(){
             <form className='flex'>
               <div className='box'>
                 <span>City</span>
-                <input type='text' placeholder='Location' />
+                <input type='text' placeholder='Location' value={selectedCity} onChange={handleCityChange}/>
               </div>
               <div className='box'>
                 <span>Room Type</span>
-                <input type='text' placeholder='Room Type' />
-              </div>
+                <input type='text' placeholder='Room Type' 
+                value={selectedRoomType} onChange={handleRoomTypeChange}/>
+                      </div>
               <div className='box'>
                 <span>Price Range</span>
-                <input type='text' placeholder='Price Range' />
-              </div>
+                <input type='text' placeholder='Price Range' 
+                value={selectedPrice} onChange={handlePriceChange}/>
+            </div>
               {/* <div className='box'>
                 <h4>Advance Filter</h4>
               </div> */}
